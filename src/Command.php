98ff87c8cda66d7a40e6bc8836e7b5626fb06a6f
Enrique Lacoma <enriquelacoma@gmail.com>
2020-03-21 06:35:33 +0100

    Fix config path

--- src/Command.php
+++ src/Command.php
@@ -50,7 +50,7 @@ class Command extends SymfonyCommand {
    * Get modules to install.
    */
   protected function getConfig() {
-    $config = Yaml::parseFile('config/config.yml');
+    $config = Yaml::parseFile(__DIR__ . '/../config/config.yml');
     $this->config = $config;
     $this->composerConfig = $config['composer']['config'];
     $this->config['project']['path'] = $config['project']['install_path'] . "/" . $config['project']['project_name'];
